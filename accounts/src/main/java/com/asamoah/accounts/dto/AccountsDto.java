aedc5ab78c71ba37857e60eec582a98f27b4e7f8
akwasiasamoah <emmanuel.asamoah@hubtel.com>
2025-01-21 22:50:26 +0000

    added dtos

---
+++ accounts/src/main/java/com/asamoah/accounts/dto/AccountsDto.java
@@ -0,0 +1,13 @@
+package com.asamoah.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
