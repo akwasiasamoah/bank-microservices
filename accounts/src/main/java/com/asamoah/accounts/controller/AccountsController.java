ed154e7f98266526b5fb74038f31ed3e436eba99
akwasiasamoah <akwasiemmanuel64@gmail.com>
2025-01-23 00:16:20 +0000

    added validation for dto properties

--- accounts/src/main/java/com/asamoah/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/asamoah/accounts/controller/AccountsController.java
@@ -6,27 +6,32 @@ import com.asamoah.accounts.dto.CustomerDto;
 import com.asamoah.accounts.dto.ResponseDto;
 import com.asamoah.accounts.service.IAccountsService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService accountsService;
 
     @PostMapping(path = "/create", consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
     @GetMapping(path = "/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
+                                                               @Pattern(regexp = "^(\\+233|0)[0-9]{9}$", message = "Mobile number should be a valid Ghanaian number and must be 10 digits")
+                                                               String mobileNumber) {
         CustomerDto customerDto = accountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
@@ -46,7 +51,9 @@ public class AccountsController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
+                                                                @Pattern(regexp = "^(\\+233|0)[0-9]{9}$", message = "Mobile number should be a valid Ghanaian number and must be 10 digits")
+                                                                String mobileNumber) {
         boolean isDeleted = accountsService.deleteAccount(mobileNumber);
         if(isDeleted) {
             return ResponseEntity
