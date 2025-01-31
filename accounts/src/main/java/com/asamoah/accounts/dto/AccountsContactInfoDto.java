52c19ae7bf8243cfa684292e81ab3d096a6d0c15
akwasiasamoah <akwasiemmanuel64@gmail.com>
2025-01-31 13:39:55 +0000

    used spring bus and web hook to update config

--- accounts/src/main/java/com/asamoah/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/asamoah/accounts/dto/AccountsContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.asamoah.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +10,11 @@ import java.util.Map;
 
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto{
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
