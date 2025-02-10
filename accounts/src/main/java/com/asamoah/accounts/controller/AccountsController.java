90c04e450408152dc27220a5ce057ec9ad04a6c6
akwasiasamoah <akwasiemmanuel64@gmail.com>
2025-02-10 07:05:03 +0000

    added rate limiter

--- accounts/src/main/java/com/asamoah/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/asamoah/accounts/controller/AccountsController.java
@@ -7,6 +7,7 @@ import com.asamoah.accounts.dto.CustomerDto;
 import com.asamoah.accounts.dto.ErrorResponseDto;
 import com.asamoah.accounts.dto.ResponseDto;
 import com.asamoah.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -225,9 +226,14 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.OK).body("Java 17");
     }
 
     @Operation(
